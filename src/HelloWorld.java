f6f40a0af4c27b77548259df4bac98af0ae416df
expandsource-and <expandsource+and@gmail.com>
2024-02-06 13:50:21 +0900

    첫번째 커밋, 자바 첫 실행, 주석 및 Hello World.

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+// 주석 : 프로그램과 상관없이 코드에 붙인 설명
+// 한줄 주석 : 단축키는 Ctrl + /
+/*
+여러줄 주석
+작성일 : 2024-02-06
+작성자 : 장원
+ */
+public class HelloWorld {  // 클래스 선언부
+    public static void main(String[] args) {    // 메소드 선언부
+        // main 메소드는 진입점 entry point
+        System.out.println("Hello, World!// 문자열에는 주석처리가 안됨" );  // 콘솔에 출력
+    } // 메소드 종료
+}  // 클래스 종료
