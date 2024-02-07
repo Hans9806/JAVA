5f5b020be0e0d6ee3415f56606eb4f0d7b9a49f5
expandsource-and <expandsource+and@gmail.com>
2024-02-07 12:03:37 +0900

    변수, 초기화 연습문제 및 풀이

---
+++ src/variable/Ex1.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // 연습문제 1 : 5와 3을 다른 숫자로 한번에 변경할 수 있게 변수로 선언해주세요.
+//        System.out.println(5 + 3);
+//        System.out.println(5 - 3);
+//        System.out.println(5 * 3);
+
+        int num1 = 5;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+
+}
+
