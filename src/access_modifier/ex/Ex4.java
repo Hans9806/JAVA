28e452be2ae5213403c83ef30e948e3d439e1900
expandsource <expandsource@gmail.com>
2024-02-21 16:29:46 +0900

    static 키워드, 메서드영역, 연습문제

--- src/access_modifier/ex/Ex4.java
+++ src/access_modifier/ex/Ex4.java
@@ -1,29 +1,37 @@
 package access_modifier.ex;
 
 public class Ex4 {
-/*
-연습 문제4.
-최대값을 지닌 카운터 만들기.
+    /*
+    연습 문제4.
+    최대값을 지닌 카운터 만들기.
 
-MaxCounter 클래스
-이 클래스는 최대값을 지정하고 최대값까지만 값이 증가하는 기능을 제공
-count : 내부에서 사용하는 숫자 입니다. 초기값은 0
-max : 최대값 입니다. 생성자를 통해 입력
-increment() 숫자를 하나 증가
-getCount() 지금까지 증가한 값을 반환하는 게터메서드
+    MaxCounter 클래스
+    이 클래스는 최대값을 지정하고 최대값까지만 값이 증가하는 기능을 제공
+    count : 내부에서 사용하는 숫자 입니다. 초기값은 0
+    max : 최대값 입니다. 생성자를 통해 입력
+    increment() 숫자를 하나 증가
+    getCount() 지금까지 증가한 값을 반환하는 게터메서드
 
-- 접근 제어자를 사용해서 데이터를 캡슐화
-- 다른 패키지에서도 사용할 수 있게 작성
+    - 접근 제어자를 사용해서 데이터를 캡슐화
+    - 다른 패키지에서도 사용할 수 있게 작성
 
-MaxCounter counter = new MaxCounter(2);
-counter.increment();
-counter.increment();
-counter.increment();
+    MaxCounter counter = new MaxCounter(2);
+    counter.increment();
+    counter.increment();
+    counter.increment();
 
-실행결과
-===
-1
-2
-최대값을 초과할 수 없습니다.
-*/
+    실행결과
+    ===
+    1
+    2
+    최대값을 초과할 수 없습니다.
+    */
+    public static void main(String[] args) {
+        MaxCounter counter = new MaxCounter(2);
+        counter.increment();
+        counter.increment();
+        counter.increment();
+//        counter.count = 10;   접근 불가하게 private으로 제어
+//        counter.max = 20;
+    }
 }
