ad6f26077b36cb5b3595b457f6a9997a62c05cfb
Hans9806 <gchan9806@gmail.com>
2024-02-22 16:05:27 +0900

    상속 연습문제

--- src/inheritance/ex/Ex2.java
+++ src/inheritance/ex/Ex2.java
@@ -2,7 +2,6 @@ package inheritance.ex;
 
 public class Ex2 {
     public static void main(String[] args) {
-        MusicPlayer musicPlayer = new MusicPlayer();
 
         ClassicalMusicTrack classicalMusicTrack = new ClassicalMusicTrack("Symphony No. 9", "Beethoven",50,"Beethoven");
         PopMusicTrack popMusicTrack = new PopMusicTrack("Shape of You", "Ed Sheeran", 23,"Divide");
@@ -10,6 +9,5 @@ public class Ex2 {
         classicalMusicTrack.play();
         popMusicTrack.play();
 
-        musicPlayer.platAll();
     }
 }
