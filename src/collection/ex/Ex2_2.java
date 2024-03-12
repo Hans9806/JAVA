298cbcde106dc89f664a397c38bd7eacff3a62ff
Hans9806 <gchan9806@gmail.com>
2024-03-12 12:42:52 +0900

    ArrayList, LinkedList, Iterator, 연습문제

---
+++ src/collection/ex/Ex2_2.java
@@ -0,0 +1,21 @@
+package collection.ex;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Ex2_2 {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.addAll(List.of("Hello", "World", "Java", "Programming"));
+        Iterator<String> iterator = strings.iterator();
+
+        while(iterator.hasNext()){
+            String s = iterator.next();
+            if (s.length() <= 5) {
+                iterator.remove();
+            }
+        }
+        System.out.println(strings);
+    }
+}
