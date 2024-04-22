d50039caba003433ee040e5be9af344cb029240e
han <gchan9806@gmail.com>
2024-04-23 01:07:49 +0900

    jdbc, 연습문제 수정

--- src/jdbc/ex/Ex5.java
+++ src/jdbc/ex/Ex5.java
@@ -1,4 +1,8 @@
 package jdbc.ex;
+
+import java.sql.*;
+import java.util.Scanner;
+
 /* 수정 출력예시
 
 사용자 ID를 입력하세요:summer
@@ -18,4 +22,67 @@ package jdbc.ex;
 - 비밀번호와 이메일주소는 기존값을 유지
 */
 public class Ex5 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String user = "root";
+        String password = "1234";
+
+        String sql = "SELECT * FROM users WHERE userId = ?";
+        String updateSql = """
+                UPDATE users SET username = ?, userPassword = ?, age = ?, email = ? WHERE userId = ? 
+                """;
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Scanner scanner = new Scanner(System.in);
+        ){
+            System.out.print("사용자 ID를 입력하세요: ");
+            String userId = scanner.nextLine();
+
+            PreparedStatement pstmt2 = conn.prepareStatement(sql);
+            pstmt2.setString(1, userId);
+            ResultSet rs = pstmt2.executeQuery();
+
+            if (rs.next()) {
+                String oldPassword = rs.getString("userPassword");
+                String oldEmail = rs.getString("email");
+
+                System.out.print("새 사용자의 이름을 입력하세요: ");
+                String username = scanner.nextLine();
+
+                System.out.print("새 비밀번호를 입력하세요: ");
+                String userPassword = scanner.nextLine();
+                if (userPassword.isEmpty()) {
+                    userPassword = oldPassword;
+                }
+
+                System.out.print("새 나이를 입력하세요: ");
+                int age = scanner.nextInt();
+
+                scanner.nextLine();
+
+                System.out.print("새 이메일 주소를 입력하세요: ");
+                String email = scanner.nextLine();
+                if (email.isEmpty()) {
+                    email = oldEmail;
+                }
+
+                PreparedStatement pstmt = conn.prepareStatement(updateSql);
+
+                pstmt.setString(1, username);
+                pstmt.setString(2, userPassword);
+                pstmt.setInt(3, age);
+                pstmt.setString(4, email);
+                pstmt.setString(5, userId);
+
+                int rows = pstmt.executeUpdate();
+                if (rows > 0) {
+                    System.out.println("사용자 ID " +userId + "의 정보가 성공적으로 업데이트되었습니다.");
+                }
+                pstmt.close();
+            }
+            pstmt2.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("에러가 발생했다" + e.getMessage());
+        }
+    }
 }
--- src/jdbc/ex/Ex6.java
+++ src/jdbc/ex/Ex6.java
@@ -1,4 +1,8 @@
 package jdbc.ex;
+
+import java.sql.*;
+import java.util.Scanner;
+
 /* 삭제 출력예시
 삭제할 사용자 ID를 입력하세요: abcdef
 해당 사용자 ID를 가진 사용자가 없습니다.
@@ -21,4 +25,50 @@ ID: spring
 사용자 ID spring의 정보가 성공적으로 삭제되었습니다.
 */
 public class Ex6 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String user = "root";
+        String password = "1234";
+
+        String selectSql = "SELECT * FROM users WHERE userId = ?";
+        String deleteSql = "DELETE FROM users WHERE userId = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Scanner scanner = new Scanner(System.in);){
+            System.out.print("삭제할 사용자 ID를 입력하세요: ");
+            String userId = scanner.nextLine();
+
+            PreparedStatement selectPstmt = conn.prepareStatement(selectSql);
+            selectPstmt.setString(1, userId);
+            ResultSet rs = selectPstmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.println("발견된 사용자 정보:");
+                System.out.println("ID: " + rs.getString("userID"));
+                System.out.println("ID: " + rs.getString("username"));
+                System.out.println("ID: " + rs.getString("email"));
+                System.out.println("ID: " + rs.getInt("age"));
+
+                System.out.print("이 사용자를 정말 삭제하시겠습니까? (y/n): ");
+                String confirm = scanner.nextLine();
+
+                if (confirm.equalsIgnoreCase("y")) {
+                    PreparedStatement deletePstmt = conn.prepareStatement(deleteSql);
+                    deletePstmt.setString(1, userId);
+                    int rows = deletePstmt.executeUpdate();
+                    if (rows > 0) {
+                        System.out.println("사용자 ID " + userId + "의 정보가 성공적으로 삭제되었습니다.");
+                    }
+                    deletePstmt.close();
+                }
+            } else {
+                System.out.println("해당 사용자 ID를 가진 사용자가 없습니다.");
+            }
+
+            selectPstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("에러가 발생했다" + e.getMessage());
+        }
+    }
 }
